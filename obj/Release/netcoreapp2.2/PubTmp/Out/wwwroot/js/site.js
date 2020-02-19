cd85bf58594b07b711f56e111ea61a691e675103
Ansar Nisar <ansarnisargill@gmail.com>
2020-02-19 11:24:17 +0500

    final office commit

---
+++ obj/Release/netcoreapp2.2/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿function Initialize() {
+    var button = document.querySelector("#consent");
+    if (button) {
+        button.addEventListener("click", function (event) {
+            document.cookie = button.dataset.cookieString;
+            document.querySelector("#consent").style.display = 'none';
+        }, false);
+    }
+}
+(function () {
+    var burger = document.querySelector('.burger');
+    var menu = document.querySelector('#' + burger.dataset.target);
+    burger.addEventListener('click', function () {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+})();
+
+
+
+function openModal(target) {
+    var $target = document.getElementById(target);
+    $target.classList.add('is-active');
+}
+
+function closeModals(target) {
+    var $target = document.getElementById(target);
+    $targrt.classList.remove('is-active');
+
+}
